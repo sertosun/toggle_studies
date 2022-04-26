031c77f0e85bfee676f41f721d33f222f73f98b3
Sadeddin Ertosun <82061290+sertosun@users.noreply.github.com>
2022-04-26 02:21:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 const text = document.querySelector(".func");
 
+
 function myFunc() {
-    text.classList.toggle("kleiner");
+    text.classList.toggle("kleiner"); 
+
+    function circularText(txt, radius, classIndex) {
+        txt = txt.split(""),
+            classIndex = document.getElementsByClassName("circTxt")[classIndex];
+    
+        var deg = 360 / txt.length,
+            origin = -90;
+    
+        txt.forEach((ea) => {
+            ea = `<p style='height:${radius}px;
+            position:absolute;
+            transform:rotate(${origin}deg);
+            transform-origin:0 100%'>${ea}</p>`;
+            classIndex.innerHTML += ea;
+            origin += deg;
+        });
+    }
+    
+    circularText("WIR SIND STARK ", 120, 0);
+
+
+
 }
+
+
+
